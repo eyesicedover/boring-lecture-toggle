233b42e92019d607a9096024ca60645aa2b69344
Masen Matthews and Stephanie Faber <stephanie@>
2018-01-09 10:38:55 -0800

    add toggle to turn on highlight and border to p tags.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $("#para1").click(function() {
+    $(".clickable").removeClass();
+    $("#para1").toggleClass("highlight");
+  });
+
+  $("#para2").click(function() {
+    $(".clickable").removeClass();
+    $("#para2").toggleClass("highlight");
+ });
+
+  $("#para3").click(function() {
+    $(".clickable").removeClass();
+    $("#para3").toggleClass("highlight");
+  });
+
+  $("#para4").click(function() {
+    $(".clickable").removeClass();
+    $("#para4").toggleClass("highlight");
+  });
+
+  $("#para5").click(function() {
+    $(".clickable").removeClass();
+    $("#para5").toggleClass("highlight");
+  });
+
+  $("#para6").click(function() {
+    $(".clickable").removeClass();
+    $("#para6").toggleClass("highlight");
+  });
+});
